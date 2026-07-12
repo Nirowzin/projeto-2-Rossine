17b779626fb415d137e23952b8f5ef28ccb44811
Nirowzin <dev@null.com>
2026-07-12 14:37:10 -0300

    fix(clevercloud): Correção para as variaveis injetadas do banco Addon

--- backend/config/database.php
+++ backend/config/database.php
@@ -86,12 +86,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('POSTGRESQL_ADDON_URI', env('DB_URL')),
+            'host' => env('POSTGRESQL_ADDON_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('POSTGRESQL_ADDON_PORT', env('DB_PORT', '5432')),
+            'database' => env('POSTGRESQL_ADDON_DB', env('DB_DATABASE', 'laravel')),
+            'username' => env('POSTGRESQL_ADDON_USER', env('DB_USERNAME', 'root')),
+            'password' => env('POSTGRESQL_ADDON_PASSWORD', env('DB_PASSWORD', '')),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
